8aa68959460ffdb4d549f690f2d4eee4f867e58f
nifan <nifan1950333426@qq.com>
2019-06-22 21:57:35 +0800

    完成header组件

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -10,10 +10,22 @@
           <span class="name">{{seller.name}}</span>
         </div>
         <div class="desc">{{seller.description}}/38分钟必达</div>
-        <div class="movement" v-if="seller.supports"><span class="icon" :class="classList[seller.supports[0].type]"></span>{{seller.supports[0].description}}</div>
+        <div class="movement" v-if="seller.supports"><span class="icon" :class="classList[seller.supports[0].type]"></span>{{seller.supports[0].description}}
+        </div>
+      </div>
+      <div class="float-wrapper-button">5个
+        <span class="icon-keyboard_arrow_right"></span>
       </div>
     </div>
-    <div class="bulletin"><span class="notice"></span>{{seller.bulletin}}</div>
+    <div class="bulletin">
+      <span class="notice">
+      </span><span class="bulletin-text">{{seller.bulletin}}</span>
+      <span class="icon-keyboard_arrow_right">
+      </span>
+    </div>
+    <div class="background">
+      <img :src="seller.avatar" width=100% height=100%>
+    </div>
     <!-- <div class="float-wrapper">
       <div class="">{{seller.name}}</div>
 
@@ -37,12 +49,24 @@ export default {
 <style lang="stylus" scoped>
 @import '~style/mixin'
   .header
+    position: relative
     color: white
+    background-color: rgba(7, 17, 27, 0.5 )
     .content
+      position: relative 
       display: flex
-      padding: 24px 12px 18px 24px
+      padding: 24px 0 18px 24px
       font-size: 0
-      background-color: rgba(7, 17, 27, 0.5 )
+      .float-wrapper-button
+        position: absolute
+        height: 24px
+        line-height : 24px
+        bottom: 14px
+        right: 12px
+        background: rgba(0,0,0,0.2)
+        border-radius: 14px
+        font-size: 10px
+        padding: 0 8px
       .content-avatar
         height: 0
         padding-bottom: 64px;
@@ -52,7 +76,7 @@ export default {
         margin-left: 18px
         font-size: 12px
         .title 
-          margin-top: 2px
+          margin-top: 2px  
           .title-img
             vertical-align: top
             display inline-block
@@ -90,30 +114,44 @@ export default {
               bg-img('invoice_1')
             &.special
               bg-img('special_1')
-
     .bulletin
+      position: relative
       line-height: 28px
-      padding: 0 12px
-      font-size: 10px
+      padding: 0 16px 0 12px
       background-color: rgba(7, 17, 27, 0.2)
-      ellipsis()  
+      ellipsis()   
       .notice
         display: inline-block
-        vertical-align: middle
+        vertical-align: top
         margin-right: 4px
+        margin-top: 7px
         bg-img('bulletin')
         width: 22px
         height: 12px
         background-size: cover
         background-repeat: no-repeat
-    // .float-wrapper
-    //   position: absolute
-    //   left: 0
-    //   right: 0
-    //   top: 0
-    //   bottom: 0
-    //   background: rgba(7,17,27,0.8)
-    //   z-index: 1
-            
-      
+      .bulletin-text
+        vertical-align: top
+        font-size: 10px
+      .icon-keyboard_arrow_right
+        position: absolute
+        right: 12px
+        top: 10px
+        font-size: 10px
+    .background
+      position: absolute
+      top: 0
+      left: 0
+      width: 100%
+      height: 100%
+      z-index: -1
+      filter: blur(10px) 
+        // .float-wrapper
+        // position: absolute
+        // left: 0
+        // right: 0
+        // top: 0
+        // bottom: 0
+        // background: rgba(7,17,27,0.8)
+        // z-index: 1
 </style>
